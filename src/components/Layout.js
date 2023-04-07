94e6182f53b89f268cd4be9658014097665b58b9
gracedward <edward.chung.designer@gmail.com>
2023-04-07 19:02:09 +0100

    FEAT: page transition

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,16 @@
 import React from "react";
+import TransitionEffect from "./TransitionEffect";
 
 const Layout = ({ children, className = "" }) => {
   return (
-    <div
-      className={`w-full h-full inline-block z-0 bg-light text-dark p-32  xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
-    >
-      {children}
-    </div>
+    <>
+      <TransitionEffect />
+      <div
+        className={`w-full h-full inline-block z-0 bg-light text-dark p-32  xl:p-24 lg:p-16 md:p-12 sm:p-8 ${className}`}
+      >
+        {children}
+      </div>
+    </>
   );
 };
 
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,10 @@ import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 
+import { AnimatePresence } from "framer-motion";
+
 import NavBar from "@/components/NavBar";
+import { useRouter } from "next/router";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -10,6 +13,7 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <>
       <Head>
@@ -20,7 +24,9 @@ export default function App({ Component, pageProps }) {
         className={`${montserrat.variable} font-mont bg-light w-full min-h-screen`}
       >
         <NavBar />
-        <Component {...pageProps} />
+        <AnimatePresence mode="wait">
+          <Component key={router.asPath} {...pageProps} />
+        </AnimatePresence>
       </main>
     </>
   );
